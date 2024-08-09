c11fdf3e4ed4ac636d0e46c0311c34bac8b2a892
Mohammad Riad Chaar <Mhdriadchaar22@gmail.com>
2024-08-09 12:46:03 +0300

    Add project files.

---
+++ DI_Service_Lifetime/Services/ScopedGUidService.cs
@@ -0,0 +1,16 @@
+﻿namespace DI_Service_Lifetime.Services
+{
+    public class ScopedGUidService : IScopedGuidService
+    {
+        private readonly Guid Id;
+        public ScopedGUidService()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public string GetGuid()
+        {
+            return Id.ToString();
+        }
+    }
+}
